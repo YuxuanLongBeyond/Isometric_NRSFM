f475315f56421575bf614ca3d9b4de33da08eee6
YuxuanLongBeyond <yuxuan.long@epfl.ch>
2020-04-02 19:14:18 +0200

    add new warping data and data synthesis algorithms

---
+++ cylinder_synthesis.m
@@ -0,0 +1,128 @@
+clear all
+close all
+
+n = 10;
+rmin = 2;
+rmax = 11;
+
+
+u = linspace(-pi/2,pi/2,20);%-1*pi/4:0.30:pi/4;
+u = repmat(u,length(u),1);
+aa = linspace(1,3,20);%0:2*0.095:1;
+aa = repmat(aa,length(aa),1);
+v = aa';
+num= linspace(rmin,rmax,n);
+
+% % Template
+x = u;
+y = v;
+
+T=[x(:),y(:)];
+
+% Make deformations
+m = 0;
+for i=length(num):-1:1
+    r= num(i);
+    x1 =  r.*sin(u/r);
+    z1 =  r.*cos(u/r);
+    %plot3(x1(:),y(:),z1(:),'*r');
+    R{i}=rodrigues(1e-1*[rand,rand,rand]);
+    
+    Tr{i} =[0, 0, -r+3];
+    P2{i}=R{i}*[x1(:), y(:),z1(:)]' + repmat(Tr{i}',1,length(x1(:)));
+    dqu = R{i}*[(z1(:)./r)';(zeros(size(z1(:))))';(-x1(:)./r)'];
+    dqv = R{i}*[(zeros(size(z1(:))))';(ones(size(z1(:))))';(zeros(size(z1(:))))'];
+    dquu = R{i}*[(-z1(:)./(r^2))';(zeros(size(z1(:))))';(-x1(:)./(r^2))'];
+    dquv = R{i}*[(zeros(size(z1(:))))';(zeros(size(z1(:))))';(zeros(size(z1(:))))'];
+    dqvv = R{i}*[(zeros(size(z1(:))))';(zeros(size(z1(:))))';(zeros(size(z1(:))))'];
+    jac{i} = [dqu; dqv];
+    hu{i} = [dquu;dquv];
+    hv{i} = [dquv;dqvv];
+    for j=1:length(jac{i})
+        nu = jac{i}(1:3,j);
+        nu = nu./norm(nu);
+        nv = jac{i}(4:6,j);
+        nv = nv./norm(nv);
+        normal = -cross(nu,nv);
+        N_phi{i}(:,j) = normal./norm(normal);
+    end
+    %plot3(P2{i}(1,:),P2{i}(2,:),P2{i}(3,:),'*r');
+end
+ %hold off
+
+% figure;
+% hold on
+% axis equal
+for i=1:length(num)
+    q{i}= [P2{i}(1,:)./P2{i}(3,:);P2{i}(2,:)./P2{i}(3,:)];%+0*randn(2,size(P2{i},2));
+    %plot(q{i}(1,:),q{i}(2,:),'*b');
+end
+%hold off
+er = 1e-3;
+nC = 100;
+t= 1e-3;
+for i=1:length(num)
+    umin = min(q{i}(1,:))-t; umax = max(q{i}(1,:))+t;
+    vmin = min(q{i}(2,:))-t; vmax = max(q{i}(2,:))+t;
+    
+    bbss = bbs_create(umin, umax, nC, vmin, vmax, nC, 3);
+    coloc = bbs_coloc(bbss, q{i}(1,:), q{i}(2,:));
+    lambdas = er*ones(nC-3, nC-3);
+    bending = bbs_bending(bbss, lambdas);
+    
+    % get control points for i to j warp
+    cpts = (coloc'*coloc + bending) \ (coloc'*P2{i}');
+    ctrlptss = cpts';
+%         [xv,yv]=meshgrid(linspace(umin,umax,100),linspace(vmin,vmax,100));
+%         ctrlpts = optimPanalSchwarz(bbs,ctrlpts,q{1}',T(:,1:2),[xv(:),yv(:)],1e-2);
+%         ctrlpts=ctrlpts';
+    q_1 = bbs_eval(bbss,ctrlptss,q{i}(1,:)',q{i}(2,:)',0,0);
+    error=sqrt(mean((q_1(1,:)-P2{i}(1,:)).^2+(q_1(2,:)-P2{i}(2,:)).^2+ (q_1(3,:)-P2{i}(3,:)).^2));
+    dqu = bbs_eval(bbss, ctrlptss, q{i}(1,:)',q{i}(2,:)',1,0);
+    dqv = bbs_eval(bbss, ctrlptss, q{i}(1,:)',q{i}(2,:)',0,1);
+    dquu = bbs_eval(bbss, ctrlptss, q{i}(1,:)',q{i}(2,:)',2,0);
+    dquv = bbs_eval(bbss, ctrlptss, q{i}(1,:)',q{i}(2,:)',1,1);
+    dqvv = bbs_eval(bbss, ctrlptss, q{i}(1,:)',q{i}(2,:)',0,2);
+    jac1{i} = [dqu; dqv];
+    hu1{i} = [dquu;dquv];
+    hv1{i} = [dquv;dqvv];
+    for j=1:length(jac{i})
+        nu = jac1{i}(1:3,j);
+        nu = nu./norm(nu);
+        nv = jac1{i}(4:6,j);
+        nv = nv./norm(nv);
+        normal = -cross(nu,nv);
+        N_phi1{i}(:,j) = normal./norm(normal);
+    end
+    disp([sprintf('[ETA] Internal Rep error = %f',error)]);
+    
+    
+end
+
+
+m = size(P2{1}, 2);
+qgth = cell(1, n);
+Pgth = zeros(3 * n, m);
+Ngth = zeros(3 * n, m);
+I2u = zeros(n - 1, m);
+I2v = zeros(n - 1, m);
+for i = 1:n
+    qgth{i} = [q{i}; ones(1, m)];
+    Pgth(((i - 1)*3+1):(3 * i), :) = P2{i};
+    Ngth(((i - 1)*3+1):(3 * i), :) = N_phi1{i};
+    if i == 1
+        I1u = q{i}(1, :); I1v = q{i}(2, :);
+    else
+        I2u(i - 1, :) = q{i}(1, :); I2v(i - 1, :) = q{i}(2, :);
+    end
+end
+
+visb = ones(n, m);
+par = 2e-3;
+I1u_all = repmat(I1u, n - 1, 1);
+I1v_all = repmat(I1v, n - 1, 1);
+[I1u,I1v,I2u,I2v,J21a,J21b,J21c,J21d,J12a,J12b,J12c,J12d,H21uua,H21uub,H21uva,H21uvb,H21vva,H21vvb] = create_warps(I1u_all,I1v_all,I2u,I2v,visb,par);
+
+save('warps_cylinder.mat', 'H21uua', 'H21uub', 'H21uva', 'H21uvb', 'H21vva', 'H21vvb', ...
+    'I1u', 'I1v', 'I2u', 'I2v', 'J21a', 'J21b', 'J21c', 'J21d', 'J12a', 'J12b', 'J12c', 'J12d', ...
+    'Ngth', 'Pgth', 'qgth');
---
+++ plane_synthesis.m
@@ -0,0 +1,109 @@
+clear all
+close all
+
+% make up an ideal camera
+f = 1000;
+cx = 0;
+cy = 0;
+
+sample_num = 20; % number of samples per row or column in the image
+range_start = -200;
+range_end = 200;
+pairs_num = 20;
+
+% define normal such that normal^T X = a1 where a1 = 1
+n1 = -[0.8, 0, 0.2]';
+n1 = n1 / norm(n1);
+d1 = 10;
+n1 = n1 / d1;
+
+total_num = sample_num ^ 2;
+
+% create image points in the first view
+[x1, y1] = meshgrid(linspace(range_start, range_end, sample_num), linspace(range_start, range_end, sample_num));
+x1 = reshape(x1, 1, total_num);
+y1 = reshape(y1, 1, total_num);
+
+K = [f, 0, cx; 0, f, cy; 0, 0, 1];
+
+q2 = K \ [x1; y1; ones(1, total_num)];
+I1u = q2(1, :);
+I1v = q2(2, :);
+
+% create a set of random rotation, translation
+t_all = randn(pairs_num, 3) * 0.5;
+r_all = randn(pairs_num, 3);
+
+% create image points in the second view
+q1 = [I1u; I1v; ones(1, total_num)];
+qgth = cell(1, pairs_num + 1);
+qgth{1} = q1;
+R_set = cell(1, pairs_num);
+I2u = zeros(pairs_num, total_num);
+I2v = zeros(pairs_num, total_num);
+H21uua = zeros(pairs_num, total_num);
+H21uub = zeros(pairs_num, total_num);
+H21uva = zeros(pairs_num, total_num);
+H21uvb = zeros(pairs_num, total_num);
+H21vva = zeros(pairs_num, total_num);
+H21vvb = zeros(pairs_num, total_num);
+J12a = zeros(pairs_num, total_num);
+J12b = zeros(pairs_num, total_num);
+J12c = zeros(pairs_num, total_num);
+J12d = zeros(pairs_num, total_num);
+J21a = zeros(pairs_num, total_num);
+J21b = zeros(pairs_num, total_num);
+J21c = zeros(pairs_num, total_num);
+J21d = zeros(pairs_num, total_num);
+Ngth = zeros(3 * (pairs_num + 1), total_num);
+Ngth(1:3, :) = repmat(n1 / norm(n1), 1, total_num);
+Pgth = zeros(3 * (pairs_num + 1), total_num);
+Pgth(1:3, :) = 1 ./ (n1' * q1) .* q1;
+for i = 1:pairs_num
+    r = r_all(i, :);
+    R = rodrigues(r);
+    t = t_all(i, :);
+    H12 = R + t' * n1';
+    q2 = H12 * q1;
+    
+    % double check if the depth is non-negative
+    while any(q2(3, :) <= 0)
+        r = randn(1, 3);
+        R = rodrigues(r);
+        H12 = R + t' * n1';
+        q2 = H12 * q1;
+    end
+    r_all(i, :) = r;
+    R_set{i} = R;
+    q2(1, :) = q2(1, :) ./ q2(3, :);
+    q2(2, :) = q2(2, :) ./ q2(3, :);
+    qgth{i + 1} = q2;
+    I2u(i, :) = q2(1, :);
+    I2v(i, :) = q2(2, :);
+    
+    n2 = R * n1;
+    a2 = (1 + n1' * R' * t');
+    H21 = R' - ((R' * t') * n2') / a2;
+    Ngth((3 * i + 1):(3 * i + 3), :) = repmat(n2 / norm(n2), 1, total_num);
+    Pgth((3 * i + 1):(3 * i + 3), :) = a2 ./ (n2' * q2) .* q2;    
+    [~, pu, pv, puu, puv, pvv]=homderivs(H21, q2(1:2, :));
+    J21a(i, :) = pu(1, :);
+    J21b(i, :) = pu(2, :);
+    J21c(i, :) = pv(1, :);
+    J21d(i, :) = pv(2, :);
+    H21uua(i, :) = puu(1, :);
+    H21uub(i, :) = puu(2, :);
+    H21uva(i, :) = puv(1, :);
+    H21uvb(i, :) = puv(2, :);
+    H21vva(i, :) = pvv(1, :);
+    H21vvb(i, :) = pvv(2, :);
+    determinant = J21a(i, :) .* J21d(i, :) - J21b(i, :) .* J21c(i, :);
+    J12a(i, :) = J21d(i, :) ./ determinant;
+    J12b(i, :) = -J21b(i, :) ./ determinant;
+    J12c(i, :) = -J21c(i, :) ./ determinant;
+    J12d(i, :) = J21a(i, :) ./ determinant;
+end
+
+save('warps_plane_steep.mat', 'H21uua', 'H21uub', 'H21uva', 'H21uvb', 'H21vva', 'H21vvb', ...
+    'I1u', 'I1v', 'I2u', 'I2v', 'J21a', 'J21b', 'J21c', 'J21d', 'J12a', 'J12b', 'J12c', 'J12d', ...
+    'Ngth', 'Pgth', 'qgth');
